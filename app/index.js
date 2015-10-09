50b48c0d974e40b608a7650ed206776d5d64440c
Andre Bossard <abossard@gmail.com>
2015-10-09 06:35:05 +0200

    more

--- app/index.js
+++ app/index.js
@@ -3,16 +3,19 @@ console.log("Hello World")
 require('jquery');
 var angular = require('angular');
 require('angular-route');
+require('angular-animate');
+
 const x = [].concat(['s']).map(i=>4)
 window.LiveReloadOptions = { host: 'localhost' };
 require('livereload-js');
 
-var app = angular.module('autodo', ['ngRoute']);
+var app = angular.module('autodo', ['ngRoute', 'ngAnimate']);
 var SimpleController = require('./controllers/SimpleController');
 var BookListController = require('./controllers/BookListController');
 var BookSingleController = require('./controllers/BookSingleController');
 var GarbageFactory = require('./factories/ProduceGarbage');
 var CounterService = require('./services/Counter');
+var ShibbyName = require('./directives/ShibbyName');
 app.constant('VERSION', require('../package.json').version)
 app.value('shibbyName', 'SHIBBY');
 app.controller('SimpleController', SimpleController)
@@ -20,6 +23,7 @@ app.controller('BookListController', BookListController)
 app.controller('BookSingleController', BookSingleController)
 app.factory('garbageFactory', GarbageFactory);
 app.service('counterService', CounterService);
+app.directive('shibbyName', ShibbyName);
 app.config(function($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'views/book_list',
